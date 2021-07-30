5272c7b2b6b1e0d0d10811d37488d8f8ab21c798
Czarek Nakamoto <cyjan@mrcyjanek.net>
2021-07-30 14:30:58 +0200

    some work with encryption

---
+++ matrix/crypto.go
@@ -0,0 +1,27 @@
+package matrix
+
+/*
+var OlmMachine *crypto.OlmMachine
+
+type Logger struct{}
+
+func (l *Logger) Error(message string, args ...interface{}) {
+	fmt.Printf(message+"\n", args...)
+}
+
+func (l *Logger) Warn(message string, args ...interface{}) {
+	fmt.Printf(message+"\n", args...)
+}
+
+func (l *Logger) Debug(message string, args ...interface{}) {
+	fmt.Printf(message+"\n", args...)
+}
+
+func (l *Logger) Trace(message string, args ...interface{}) {
+	fmt.Printf(message+"\n", args...)
+}
+
+func Encrypt() {
+	crypto.NewOlmMachine(Client, Logger{}, db.Store)
+}
+*/
