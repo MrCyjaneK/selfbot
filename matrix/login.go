d7f2409558ae44af8f5de9ff4882ad059b17d981
Carlod <carlod@disroot.org>
2021-07-31 09:54:46 +0200

    Fixed homeserver errors when logging in

--- matrix/login.go
+++ matrix/login.go
@@ -3,7 +3,9 @@ package matrix
 import (
 	"fmt"
 	"strings"
-
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
 	"git.mrcyjanek.net/mrcyjanek/selfbot/db"
 	"maunium.net/go/mautrix"
 	"maunium.net/go/mautrix/id"
@@ -19,8 +21,16 @@ func Login() error {
 			homeserver := Ask("Homeserver (eg. mrcyjanek.net)")
 			username := Ask("Username (eg. cyjan)")
 			password := Ask("Password (eg. ***** *** * **)")
-			var err error
-			Client, err = mautrix.NewClient(homeserver, "", "")
+			url := "https://"+homeserver+"/.well-known/matrix/client"
+			res, err := http.Get(url)
+			if err != nil {
+			}
+			body, err := ioutil.ReadAll(res.Body)
+			var content map[string] map[string] interface{}
+			json.Unmarshal(body, &content)
+			homeserver2 := content["m.homeserver"]["base_url"].(string)
+			homeserver2 = strings.Split(homeserver, "/")[2]
+			Client, err := mautrix.NewClient(homeserver2, "", "")
 			if err != nil {
 				return err
 			}
@@ -39,7 +49,7 @@ func Login() error {
 			fmt.Println("accesstoken:", resp.AccessToken)
 			fmt.Println("deviceid:", resp.DeviceID)
 			fmt.Println("userid:", resp.UserID)
-			db.Set("meta.homeserver", []byte(homeserver))        // mrcyjanek.net
+			db.Set("meta.homeserver", []byte(homeserver2))        // mrcyjanek.net
 			db.Set("meta.accesstoken", []byte(resp.AccessToken)) // ZaI......................JRY
 			db.Set("meta.deviceid", []byte(resp.DeviceID))       // bt7s33Z2
 			db.Set("meta.userid", []byte(resp.UserID))           // @cyjan:mrcyjanek.net
@@ -47,11 +57,24 @@ func Login() error {
 			homeserver := Ask("Homeserver (eg. mrcyjanek.net)")
 			accesstoken := Ask("Access token (eg. dijbejbfaicpsbigwkcgauie)")
 			username := Ask("Username (eg. cyjan)")
-			db.Set("meta.homeserver", []byte(homeserver))
+			url := "https://"+homeserver+"/.well-known/matrix/client"
+			res, err := http.Get(url)
+			body, err := ioutil.ReadAll(res.Body)
+			var content map[string] map[string] interface{}
+			json.Unmarshal(body, &content)
+			homeserver2 := content["m.homeserver"]["base_url"].(string)
+			homeserver2 = strings.Split(homeserver, "/")[2]
+			db.Set("meta.homeserver", []byte(homeserver2))
 			db.Set("meta.accesstoken", []byte(accesstoken))
 			db.Set("meta.userid", []byte("@"+username+":"+homeserver))
-			var err error
-			Client, err = mautrix.NewClient(homeserver, id.NewUserID(username, homeserver), accesstoken)
+						if err != nil {
+				return err
+			}
+			if homeserver == "mozilla.modular.im" {
+				Client, err = mautrix.NewClient(homeserver, id.NewUserID(username, "mozilla.org"), accesstoken)
+			} else {
+				Client, err = mautrix.NewClient(homeserver, id.NewUserID(username, homeserver), accesstoken)
+			}
 			if err != nil {
 				return err
 			}
@@ -60,9 +83,12 @@ func Login() error {
 	homeserver := string(db.Get("meta.homeserver"))
 	accesstoken := string(db.Get("meta.accesstoken"))
 	username := strings.Split(strings.Split(string(db.Get("meta.userid")), ":")[0], "@")[1]
-
 	var err error
-	Client, err = mautrix.NewClient(homeserver, id.NewUserID(username, homeserver), accesstoken)
+	if homeserver == "mozilla.modular.im" {
+				Client, err = mautrix.NewClient(homeserver, id.NewUserID(username, "mozilla.org"), accesstoken)
+			} else {
+				Client, err = mautrix.NewClient(homeserver, id.NewUserID(username, homeserver), accesstoken)
+			}
 	return err
 }
 
